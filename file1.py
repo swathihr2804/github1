b2857829e6dca8cb9fcc3d1ae75fc2c1f7d48643
swathih <swathi.hreddy@gmail.com>
2025-09-15 13:00:10 +0530

    first commit

---
+++ file1.py
@@ -0,0 +1,24 @@
+# def add(a,b):
+#     return (a+b)
+# print(add(10,11))
+
+# var = [10,11,12,13,1,7,1,9,]
+# var.append([8,9])
+# print(var)
+
+
+
+# var = [10,11,12,13,1,7,1,9,]
+# var.insert(1,[3,2])
+# print(var)
+
+# var = [10,11,12,13,1,7,1,9,]
+# var.sort()
+# print(var)
+
+
+
+greetings = "swathi"
+greetings1 = " hello how are you?"
+message = greetings + greetings1
+print(len(message))
